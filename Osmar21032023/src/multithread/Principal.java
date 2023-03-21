74a5eea835ec6f6a3d69ed8e52d0dd50f6693bc5
RustyPPsH <108179227+RustyPPsH@users.noreply.github.com>
2023-03-21 20:50:07 -0300

    Add files via upload

---
+++ Osmar21032023/src/multithread/Principal.java
@@ -0,0 +1,10 @@
+package multithread;
+
+public class Principal {
+public static void main(String args[ ]) {
+    System.out.println("Antes da Janela:" + Thread.activeCount());
+    MultiplaThread janela = new MultiplaThread();
+    janela.setVisible(true);
+    System.out.println("Depois da Janela:" + Thread.activeCount());
+    }
+}
---
+++ Osmar21032023/src/src/Principal.java
@@ -0,0 +1,8 @@
+package src;
+
+public class Principal {
+public static void main(String[] args) {
+System.out.println("Numero de threads:" + Thread.activeCount());
+System.out.println(Thread.currentThread());
+    }
+} 
